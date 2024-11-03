4924732d9486492bf3dc578dafe98e6b179117c7
Adrian Talbot <adrian.r.talbot@gmail.com>
2024-11-03 21:19:21 +0000

    Initial Commit

---
+++ LocateMe/LocateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocateMeApp.swift
+//  LocateMe
+//
+//  Created by Adrian Talbot on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
